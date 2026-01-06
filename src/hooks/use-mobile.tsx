53d878c0bfe5d67a91d7701b455e6c2060ab668f
kkmanuu <kipngenoemmanuel479@gmail.com>
2026-01-06 10:25:11 +0300

    Add useIsMobile hook to detect mobile screens

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,26 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768;
+const MOBILE_BREAKPOINT = 768; // width threshold for mobile
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+
+    // Update state when screen size changes
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
+
     mql.addEventListener("change", onChange);
+
+    // Set initial value
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+
+    // Cleanup listener
     return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return !!isMobile;
+  return !!isMobile; // always return boolean
 }
