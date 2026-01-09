420c509bf64a1c1bc4aa8782b1f0e488ff8741a5
kkmanuu <kipngenoemmanuel479@gmail.com>
2026-01-09 09:59:40 +0300

    docs: add comments to cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility function for combining Tailwind CSS class names.
+ * - Uses `clsx` for conditional class names
+ * - Uses `twMerge` to merge and deduplicate Tailwind classes
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
